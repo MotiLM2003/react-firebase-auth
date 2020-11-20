88986ec5879620ec1962d9f534c40b3598198bb0
Moti Elmakies <motiphone2003@gmail.com>
2020-11-21 01:14:50 +0200

    :fire: firebase  signup authentication completed

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,16 +1,32 @@
 import React, { useRef } from 'react';
 import { Form, Button, Card } from 'react-bootstrap';
+import { useAuth } from '../contexts/AuthContext';
 
 const Signup = () => {
   const emailRef = useRef();
   const passwordRef = useRef();
   const passwordConfirmRef = useRef();
+  const { signUp } = useAuth();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (passwordRef.current.value === passwordConfirmRef.current.value) {
+      try {
+        await signUp(emailRef.current.value, passwordRef.current.value);
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      console.log('not match');
+    }
+  };
+
   return (
     <React.Fragment>
       <Card>
         <Card.Body>
-          <h2 className='text-center mb-4'>Sign Up</h2>
-          <Form>
+          <h2 className='text-center mb-4 '>Sign Up</h2>
+          <Form onSubmit={handleSubmit}>
             <Form.Group id='email'>
               <Form.Label>Email</Form.Label>
               <Form.Control type='email' required ref={emailRef} />
